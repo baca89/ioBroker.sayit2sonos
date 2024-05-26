4e3a940efe7d3ce88318b43d2dad1e73a04cdeab
Carsten Bauer <info@bauerc.eu>
2024-05-26 13:47:40 +0000

    added Ste for text-input

--- main.js
+++ main.js
@@ -58,24 +58,24 @@ class Sayit2sonos extends utils.Adapter {
 		Here a simple template for a boolean variable named "testVariable"
 		Because every adapter instance uses its own unique namespace variable names can't collide with other adapters variables
 		*/
-		// await this.setObjectNotExistsAsync('testVariable', {
-		// 	type: 'state',
-		// 	common: {
-		// 		name: 'testVariable',
-		// 		type: 'boolean',
-		// 		role: 'indicator',
-		// 		read: true,
-		// 		write: true,
-		// 	},
-		// 	native: {},
-		// });
+		await this.setObjectNotExistsAsync('text', {
+			type: 'state',
+			common: {
+				name: 'Text that has to be spoken',
+				type: 'string',
+				role: 'text',
+				read: true,
+				write: true,
+			},
+			native: {},
+		});
 
 		// In order to get state updates, you need to subscribe to them. The following line adds a subscription for our variable we have created above.
 		// this.subscribeStates('testVariable');
 		// You can also add a subscription for multiple states. The following line watches all states starting with "lights."
 		// this.subscribeStates('lights.*');
 		// Or, if you really must, you can also watch all states. Don't do this if you don't need to. Otherwise this will cause a lot of unnecessary load on the system:
-		// this.subscribeStates('*');
+		this.subscribeStates('*');
 
 		/*
 			setState examples
@@ -141,8 +141,10 @@ class Sayit2sonos extends utils.Adapter {
 	 */
 	onStateChange(id, state) {
 		if (state) {
-			// The state was changed
-			this.log.info(`state ${id} changed: ${state.val} (ack = ${state.ack})`);
+			if (!state.ack) {
+				this.log.info(`Text to be spoken : ${state.val}`);
+			}
+			this.log.debug(`state ${id} changed: ${state.val} (ack = ${state.ack})`);
 		} else {
 			// The state was deleted
 			this.log.info(`state ${id} deleted`);
